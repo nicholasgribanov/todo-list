0c80a90f0d02f87466d7948a65433fa33fce1639
Nikolay Gribanov <nicholasg.ix35@gmail.com>
2020-02-03 07:49:53 +0300

    Extract class Todo Added routing from welcome to todo's list

--- src/app/list-todo/list-todo.component.ts
+++ src/app/list-todo/list-todo.component.ts
@@ -1,5 +1,15 @@
 import {Component, OnInit} from '@angular/core';
 
+export class Todo {
+  constructor(
+    public id: number,
+    public description: string,
+    public targetDate: Date,
+    public done: boolean
+  ) {
+  }
+}
+
 @Component({
   selector: 'app-list-todo',
   templateUrl: './list-todo.component.html',
@@ -8,9 +18,9 @@ import {Component, OnInit} from '@angular/core';
 export class ListTodoComponent implements OnInit {
 
   todos = [
-    {id: 1, description: "Learn Angular"},
-    {id: 2, description: "Visit Mexico"},
-    {id: 3, description: "Drink tea"}
+    new Todo(1,"Learn Angular", new Date(), false),
+    new Todo(2,"Visit Mexico", new Date(), false),
+    new Todo(3,"Drink tea", new Date(), false),
   ];
 
   constructor() {
